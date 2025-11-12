b8c5b83e35bc70d596ca69150f0d1b9f2a7af1be
JulioGaspareto <juliogaspareto0@gmail.com>
2025-11-11 22:05:58 -0300

    finalizando CEP

--- localmarket/src/components/Navbar2.jsx
+++ localmarket/src/components/Navbar2.jsx
@@ -2,14 +2,14 @@ import './Navbar2.css'
 
 function Navbar2() {
   return (
-    <div className='Navbar-container'>
-        <div className='navbar-left'>
-            <img className='logoNav' src="./img/LogoAtu.svg" alt="" />
+    <div className='Navbar-container-2'>
+        <div className='navbar-left-2'>
+            <img className='logoNav-2' src="./img/LogoAtu.svg" alt="" />
         </div>
-        <div className='navbar-center'>
+        <div className='navbar-center-2'>
             <h1>Local Market</h1>
         </div>
-        <div className='navbar-spacer'></div>
+        <div className='navbar-spacer-2'></div>
     </div>
   )
 }
