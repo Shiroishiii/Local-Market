8b359f30f4a4dd163c55c860c0041dd8e2b14c26
Isaque-cyber <isaque_r_silva@estudante.sesisenai.org.br>
2025-11-18 19:51:59 -0300

    Criação da tela pra anunciar

---
+++ localmarket/src/pages/Anunciar.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+function Anunciar() {
+  return (
+    <div className='container'>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    </div>
+  )
+}
+
+export default Anunciar
